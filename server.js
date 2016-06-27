3647cb9c6c8ee8dc561789b8a84f05c600e491fd
Michael Ray <raymichael23@gmail.com>
2016-06-27 10:46:46 -0700

    added server listening and mongoose connection

--- server.js
+++ server.js
@@ -3,4 +3,16 @@ var
   bodyParser = require('body-parser'),
   express = require('express'),
   app = express(),
-  logger = require('morgan')
+  logger = require('morgan'),
+  PORT = process.env.PORT || 3000,
+  
+
+mongoose.connect('MongoDB://localhost/chetflix_and_nil', function(err) {
+  if (err) throw err;
+  console.log('Connected to MongoDB (chetflix_and_nil)')
+})
+
+app.listen(PORT, function(err) {
+  if (err) throw err;
+  console.log('Server listening on port:', PORT)
+})
