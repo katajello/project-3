bf1e88fd99776001fbc8c3dd42ee9ff8db01a8cf
Ed Petil <e.petil.ms@gmail.com>
2016-06-27 11:13:32 -0700

    started intial api

--- server.js
+++ server.js
@@ -3,4 +3,31 @@ var
   bodyParser = require('body-parser'),
   express = require('express'),
   app = express(),
-  logger = require('morgan')
+  logger = require('morgan'),
+  var PORT = process.env.PORT || 3000;
+
+  mongoose.connect('mongodb://localhost/project-3', function(err) {
+  	if(err) return console.log(err)
+  	console.log("Connected to MongoDB (project-3)")
+  })
+
+  app.get('/chetflixsandbil/:id', function(req, res) {
+    var apiUrl = "http://pokeapi.co/api/v2/pokemon/" + req.params.id
+
+    request(apiUrl, function(err, response) {
+      // var image = JSON.parse(response.body).data
+      var data = JSON.parse(response.body)
+      var pokemon = {
+        name: data.name,
+        weight: data.weight,
+        height: data.height,
+        base_experience: data.base_experience
+      }
+
+      res.json(pokemon)
+    })
+  })
+
+  app.listen(PORT, function() {
+    console.log('Server is runing on port 3000')
+  })
