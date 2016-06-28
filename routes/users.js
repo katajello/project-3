c4d7d54d33097d68ec4941838bd3d8ac4f63a443
katieO <katajello@yahoo.com>
2016-06-27 17:21:30 -0700

    added user.id patch

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var
   express = require('express'),
   passport = require('passport'),
+  User = require('../models/User.js'),
   userRouter = express.Router()
 
 userRouter.route('/login')
@@ -21,6 +22,31 @@ userRouter.route('/signup')
     successRedirect: '/profile',
     failureRedirect: '/signup'
   }))
+
+userRouter.route('/users')
+.get(function (req, res) {
+  User.find({}, function (err, users) {
+    if (err) return console.log(err);
+    res.json(users)
+  })
+})
+
+userRouter.route('/users/:id')
+  .get(function (req, res) {
+    User.findById(req.params.id, function (err, user) {
+      if(err) return console.log(err);
+      res.json(user)
+    })
+  })
+  .patch(function (req, res) {
+      User.findByIdAndUpdate(req.params.id, {local:req.body}, {new:true}, function (err, user) {
+        if (err) return console.log(err);
+        res.json({success:true, message:"User Updated", user: user})
+        // res.json(req.params.id)
+      })
+    })
+
+
 
 userRouter.get('/profile', isLoggedIn, function (req, res) {
   res.render('profile', {user: req.user})
